0dcabe20a04f0e9aa658f1c1077cfeb1c99a417b
Soumyabrata Bhattacharjee <soumyabrata111@gmail.com>
2023-10-25 22:30:15 +0530

    Update Exhaustive_Search_Method.py

--- Python/Exhaustive_Search_Method.py
+++ Python/Exhaustive_Search_Method.py
@@ -14,18 +14,17 @@ def f(x):
 x1=a
 x2=x1+dx
 x3=x2+dx
+x_min=x2
+f_min =f(x_min)
 
 while x3<=b:
-    if f(x2)<=f(x1) and f(x2)<f(x3):
+    if f(x2)<=f(x1) and f(x2)<=f(x3):
         print('The minimum point lies between: ['+str(x1)+','+str(x3)+']')
         print('The approx minimum point is: '+str(x2))
         print('The approx minimum function value is: '+str(f(x2)))
         x_min=x2
         f_min=f(x_min)
-        x_lb=x1
-        f_lb=f(x_lb)
-        x_ub=x2
-        f_ub=f(x_ub)
+       
     x1=x2
     x2=x1+dx
     x3=x2+dx
