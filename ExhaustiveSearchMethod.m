1788d9a48c5e1528b9f807cadbfe61f70cdca015
Soumyabrata Bhattacharjee <soumyabrata111@gmail.com>
2023-10-25 22:58:41 +0530

    Update ExhaustiveSearchMethod.m

--- ExhaustiveSearchMethod.m
+++ ExhaustiveSearchMethod.m
@@ -3,40 +3,60 @@
 % Code developed by Sri. Soumyabrata Bhattacharjee
 % Matlab version R2018b
 % Date: 14th October, 2018
+% Date: 25th October, 2023 (Updated)
+
+
+clear all
 clc
-clear
-%% Taking input from user
-a = input('What is the lower limit? ');
-b = input('What is the upper limit? ');
-n = input('What is the number of intermediate points? ');
-delta_x = (b-a)/n; % Increment in x
-c = max(a,b) % To set the axex limit
+a=0.1; %Lower bound
+b=14; % Upper bound
+n = 1000; %Number of divisions
+dx=(b-a)/n; %Length of each division
 
-%% Initialization
-x1 = a;
-x2 = x1 + delta_x; 
-x3 = x2 + delta_x; 
+% Equation to be minimized
+f=@(x) x.^2+54./x;
 
-%% Taking thefunction as input from user
-str = input('Give an equation in x: ','s'); % the user types in, for instance 2*x^2-3*x+4
-f = inline(str,'x') ;
+% Initialization
+x1=a;
+x2=x1+dx;
+x3=x2+dx;
+x_min=x2;
+f_min=f(x_min);
 
-%% Evaluating the function for x1, x2 & x3
-while (x3 <= b)
-    y1 = feval(f,x1); 
-    y2 = feval(f,x2);
-    y3 = feval(f,x3); 
-    if y1>y2 && y2<y3 
-        fprintf('The solution lies betwee %f & %f \n The solution is (approximately)%f: \n',x1,x3,x2);
-        break
-    else
-        x1 = x2;
-        x2 = x3;
-        x3 = x2 + delta_x;
+while x3<=b
+    if f(x2)<=f(x1) && f(x2)<=f(x3)
+        disp(['The minimum point lies between: [' num2str(x1) ',' num2str(x3) ']'])
+        disp(['The approx minimum point is: ' num2str(x2)])
+        disp(['The approx minimum function value is: ' num2str(f(x2))])
+        x_min=x2;
+        f_min=f(x_min);
     end
-    fplot (f,[a b], 'b')% Plotting the function
-    grid on
-    hold on
-    plot(x2,y2,'*') % Plotting the minimum pooint
-    hold off
+x1=x2;
+x2=x1+dx;
+x3=x2+dx;
+
 end
+
+
+% Plotting
+x = linspace(a,b,n);
+plot(x,f(x),'LineWidth',2,'DisplayName',func2str(f));
+xlabel('x',FontWeight='bold');
+ylabel('f(x)','FontWeight','bold')
+grid on;
+title('Exhaustive Search Method','FontWeight','bold');
+
+hold on;
+
+% Scatter plot for the minimum point
+scatter(x_min, f_min, 100, 'red', 'filled', 'DisplayName', 'Minimum Point');
+
+% Adding Legend
+legend('Location','best')
+legend('show')
+
+% Save the figure as a PNG file
+saveas(gcf, 'Exhaustive_Search_Method.png');
+
+% Display the plot
+hold off;
