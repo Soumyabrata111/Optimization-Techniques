dd242b575a1176847eab008815cca648e0793e04
Soumyabrata Bhattacharjee <soumyabrata111@gmail.com>
2023-10-29 16:49:47 +0530

    It is still not working BoundingPhaseMethod.py

--- BoundingPhaseMethod.py
+++ BoundingPhaseMethod.py
@@ -5,46 +5,47 @@ a = 0.1  # Lower Bound
 b = 14  # Upper Bound
 n = 1000  # Number of desired partitions in the interval
 dx = (b - a) / n  # Length of each such partition
-accur = 1e-5
 
 # Initial value of x
-x0 = a + 0.1 * dx
+x0 = a + 0.1 * (b - a)
 
 # Function to be minimized
 def f(x):
     return x**2 + 54/x
 
-# Fixing the sign of dx
-if f(x0 - dx) >= f(x0) and f(x0 + dx) <= f(x0):
+print(f"a {a},b {b}, x0 {x0}")
+
+# Decide the value of dx
+if f(x0-dx) >= f(x0) and f(x0) >= f(x0+dx):
     dx = abs(dx)
-elif f(x0) <= f(x0 - dx) and f(x0) <= f(x0 + dx):
+else:
     dx = -abs(dx)
 
-print(f"dx = {dx}")
+k = 0
+x_n = x0 + 2**k * dx
+x_lb = x_n - dx
+x_ub = x_n + dx
 
-# Initialization
-k = 0  # Number to calculate new value of x
-x_new = x0 + 2**k * dx
-f_lb = f(x_new - dx)
-f0 = f(x_new)
-f_up = f(x_new + dx)
+f_n = f(x_n)
+f_lb = f(x_lb)
+f_ub = f(x_ub)
 
-x_min = None  # Initialize x_min
-
-while x_new >= a and x_new <= b:
-    if f0 <= f_lb and f0 <= f_up:
-        x_min = x_new
-        f_min = f(x_min)
+while x_n >= a and x_n <= b:
+    if f_n <= f_lb and f_n <= f_ub:
+        x_min = x_n
+        f_min = f_n
         break  # You found the minimum, so break out of the loop
     else:
         k = k + 1
-        x_new = x_new + 2**k * dx  # Update x_new using the correct value
-        f_lb = f(x_new - dx)
-        f0 = f(x_new)
-        f_up = f(x_new + dx)
-        print(f"New value of x is {x_new}")
+        x_n = x0 + 2**k * dx
+        x_lb = x_n - dx
+        x_ub = x_n + dx
+        f_n = f(x_n)
+        f_lb = f(x_lb)
+        f_ub = f(x_ub)
+        print(f"x_n: {x_n}")
 
-if x_min is not None:
+if 'x_min' in locals():
     print(f"The minimum value is at {x_min} and the minimum value is {f_min}")
 else:
     print("No minimum found within the specified bounds.")
