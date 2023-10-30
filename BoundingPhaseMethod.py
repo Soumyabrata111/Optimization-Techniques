d6da6e0cf96daeb4492e10ac176a8d8477746c76
Soumyabrata Bhattacharjee <soumyabrata111@gmail.com>
2023-10-30 20:26:38 +0530

    Update BoundingPhaseMethod.py

--- BoundingPhaseMethod.py
+++ BoundingPhaseMethod.py
@@ -1,51 +1,46 @@
-#NOT WORKING
-
 # Bounding Phase Method
-a = 0.1  # Lower Bound
-b = 14  # Upper Bound
-n = 1000  # Number of desired partitions in the interval
-dx = (b - a) / n  # Length of each such partition
 
-# Initial value of x
-x0 = a + 0.1 * (b - a)
+import matplotlib.pyplot as plt
+import numpy as np
+a = 0.1  # Lower bound
+b = 14   # Upper bound
+dx=1e-8 # Step=size parameter
 
 # Function to be minimized
 def f(x):
-    return x**2 + 54/x
+    return x ** 2 + 54 / x
 
-print(f"a {a},b {b}, x0 {x0}")
+# Initialization
+x_mid=a+dx*(b-a) # Initial guess
+x_lb=x_mid-dx # Current lower bound
+x_ub=x_mid+dx # Current upper bound
+f_mid = f(x_mid)
+f_lb=f(x_lb)
+f_ub=f(x_ub)
+k=0 # Value will be iterated
 
-# Decide the value of dx
-if f(x0-dx) >= f(x0) and f(x0) >= f(x0+dx):
-    dx = abs(dx)
-else:
-    dx = -abs(dx)
+# Check whether the function is unimodal
 
-k = 0
-x_n = x0 + 2**k * dx
-x_lb = x_n - dx
-x_ub = x_n + dx
-
-f_n = f(x_n)
-f_lb = f(x_lb)
-f_ub = f(x_ub)
-
-while x_n >= a and x_n <= b:
-    if f_n <= f_lb and f_n <= f_ub:
-        x_min = x_n
-        f_min = f_n
-        break  # You found the minimum, so break out of the loop
+while x_ub<=b and x_lb >=a:
+    if f_mid>=f_lb and f_mid>=f_ub:
+        print(f_lb,f_mid,f_ub)
+        print("The function is not unimodal")
+    elif f_lb>=f_mid and f_mid>=f_ub:
+        dx=abs(dx)
+        print(f"dx: {dx}")
     else:
-        k = k + 1
-        x_n = x0 + 2**k * dx
-        x_lb = x_n - dx
-        x_ub = x_n + dx
-        f_n = f(x_n)
-        f_lb = f(x_lb)
-        f_ub = f(x_ub)
-        print(f"x_n: {x_n}")
-
-if 'x_min' in locals():
-    print(f"The minimum value is at {x_min} and the minimum value is {f_min}")
-else:
-    print("No minimum found within the specified bounds.")
+        dx=-abs(dx)
+        print(f"dx: {dx}")
+    k=k+1
+    x_mid_new = x_mid + 2**k*dx
+    f_mid_new = f(x_mid_new)
+    if f_mid_new > f_mid:
+        break
+    x_mid = x_mid_new
+    x_lb=x_mid-dx
+    x_ub=x_mid+dx
+    f_mid = f_mid_new
+    f_lb=f(x_lb)
+    f_ub=f(x_ub)
+    print(f"k={k},x_mid={x_mid},x_lb={x_lb},x_ub={x_ub}")
+    print (f"The minimum value of the function, {f_mid}, is obtained at {x_mid}")
