d4006aef7bc3ba3c44db2bf849049b2fd3b65d84
Soumyabrata Bhattacharjee <soumyabrata111@gmail.com>
2023-10-31 16:58:26 +0530

    Update IntervalHalvingMethod.py

--- IntervalHalvingMethod.py
+++ IntervalHalvingMethod.py
@@ -18,3 +18,22 @@ f2 = f(x2)
 fm = f(xm)
 print(f"x1={x1}, xm={xm}, x2={x2}")
 print(f"f1={f1}, fm={fm}, f2={f2}")
+
+while abs(L)>eps:
+    if f1<fm:
+        b=xm
+        xm=x1
+    elif f2<fm:
+        a=xm
+        xm=x2
+    else:  
+        a=x1
+        b=x2
+    L=b-a
+    x1=a+L/4
+    x2=b-L/4
+    f1 = f(x1)
+    f2 = f(x2)
+    fm = f(xm)
+    
+print (f"The minimum point lies between {x1} and {x2}")
