08a9efc5eb6a9abd5e2e9008113fa636ac285700
Soumyabrata Bhattacharjee <soumyabrata111@gmail.com>
2023-10-31 16:40:05 +0530

    Update IntervalHalvingMethod.py

--- IntervalHalvingMethod.py
+++ IntervalHalvingMethod.py
@@ -6,3 +6,9 @@ eps = 1e-5 # Desired accuracy
 # Function to minimize
 def f(x):
     return x**2+54/x
+
+# Initialization
+x_m=(a+b)/2 # Mid point
+L=b-a # Length of search space
+x1=a+L/4 # Left intermediary point 
+x2=b-L/4 # Right intermediary point
