dcd417cb710159c6cbbf861f0299cf7cb9140025
Soumyabrata Bhattacharjee <soumyabrata111@gmail.com>
2023-11-02 20:41:59 +0530

    Update fibonacci_search_method.m

--- fibonacci_search_method.m
+++ fibonacci_search_method.m
@@ -1,61 +1,79 @@
 % Fibonacci Search Method
 % Matlab version R2018b
 % Date: 10th February, 2019 (started)
+% Date: 2nd November, 2023 (updated)
+
 clc
 clear
-%% Taking thefunction as input from user
-sc = inputdlg('Type an expression that is a function of x ' );    % Taking the equation as input from the user
-s = sc{:};                                                            % Function String
-f = str2func(['@(x) ' s])
-a = input ('Enter lower boundary point: ');
-b = input ('Enter upper boundary point: ');
-n = input ('Enter the desired number of function evaluation (greater than 2): ');
+a = 0.1; % Lower bound
+b = 14; % Upper bound
+n = 1000; % Number of times function is to be evaluated
+b_p=b;
+a_p=a;
 
-%% Initiating required variables
+% Function to be minimized
+f = @(x) x.^2 + 54./x;
+
+% Initialization
 k = 2;
-L = b - a;
-y1 = feval(f,(a));
-y2 = feval(f,(b));
-% %% Starting the algorithm
+L = b - a; % Length of search space
+L_ks = (F(n-k+1)/F(n+1)) * L;
+x1 = a + L_ks;
+x2 = b - L_ks;
+f1 = f(x1);
+f2 = f(x2);
+
 while k <= n
-    x_1 = n-k+1;
-    f_x_1 = fibonn(n-k+1);
-    f_x_2 = fibonn(n+1);
-    Lk_star = (f_x_1/f_x_2)* L
-    x1 = a + Lk_star;
-    x2 = b - Lk_star;
-    if mod (k,2) == 0
-        y1 = feval(f,(x1));
-    else
-        y2 = feval(f,(x2));
-    end
-    if y1 > y2
+    if f2 < f1
         a = x1;
     else
         b = x2;
     end
-    c = 0.5*(a+b);
+    L = b - a;
+    L_ks = (F(n - k + 1) / F(n + 1)) * L; % Recalculate L_ks
+    x1 = a + L_ks;
+    x2 = b - L_ks;
+    f1 = f(x1);
+    f2 = f(x2);
     k = k + 1;
 end
-a
-b
-fprintf ('The minimum value is: %f \n',c)
 
-%% Fibonacci number function
-function f_x = fibonn(x)
-if x == 0
-    f_x = 1;
-elseif x == 1
-    f_x = 1;
-else
-    a = 1;
-    b = 1;
-    for i = 2:x
-        f_x = a + b;
-        a = b;
-        b = f_x;
+x_min = (x1 + x2) / 2;
+f_min = f((x1 + x2) / 2);
+
+fprintf('The approximate minimum point and the value respectively are: %f and %f\n', x_min, f_min);
+
+% Plot the function
+x_values = linspace(0, b_p, 1000); % Update here
+y_values = f(x_values);
+figure;
+plot(x_values, y_values);
+xlabel('x', 'fontweight', 'bold');
+ylabel('f(x)', 'fontweight', 'bold');
+grid on;
+title('Fibonacci Search Method', 'fontweight', 'bold');
+hold on;
+scatter(x_min, f_min, 'red', 'filled');
+legend(func2str(f), 'Approximate Minimum Point');
+
+% Set x-axis ticks to be whole numbers
+xticks(a_p:b_p);
+
+% Adjust x-axis limits
+ax = gca;
+ax.XLim = [a_p b_p]; % Update here
+
+saveas(gcf, 'Fibonacci_Search_Method.png', 'png');
+
+% Function to generate n-th Fibonacci number
+function result = F(n)
+    fi = zeros(1, n+1);
+    fi(1) = 0;
+    fi(2) = 1;
+
+    for i = 3:n+1
+        fi(i) = fi(i-1) + fi(i-2);
     end
-end
-end
 
-
+    result = fi(n+1);
+end
